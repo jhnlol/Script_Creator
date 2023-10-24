5fde94e46c241a3080bd5641bc5dac934e629c78
jhnlol <78961113+jhnlol@users.noreply.github.com>
2023-10-24 13:50:19 +0200

    Add author

--- Script_Creator/Program.cs
+++ Script_Creator/Program.cs
@@ -1,9 +1,9 @@
-﻿using System;
+using System;
 using System.IO;
 
 class Program
 {
-    static void CreateFoldersAndFiles(string folderName, string location)
+    static void CreateFoldersAndFiles(string folderName, string location, string author)
     {
         string folderPath = Path.Combine(location, folderName);
         Directory.CreateDirectory(folderPath);
@@ -16,7 +16,7 @@ class Program
         File.WriteAllText(Path.Combine(folderPath, "config.lua"), "Config = {}");
         File.WriteAllText(Path.Combine(folderPath, "fxmanifest.lua"), $@"fx_version 'cerulean'
 game 'gta5'
-author 'JHN'
+author '{author}'
 shared_script 'config.lua'
 client_script 'client/client.lua'
 server_script 'server/server.lua'
@@ -30,8 +30,9 @@ server_script 'server/server.lua'
 
         Console.Write("Podaj lokalizację, w której chcesz utworzyć folder i pliki: ");
         string location = Console.ReadLine();
-
-        CreateFoldersAndFiles(folderName, location);
+        Console.Write("Podaj nautora skryptu: ");
+        string author = Console.ReadLine();
+        CreateFoldersAndFiles(folderName, location, author);
 
         Console.WriteLine("Struktura folderu i plików została utworzona.");
     }
